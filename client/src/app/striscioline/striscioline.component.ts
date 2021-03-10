586a2d8473fa6a780bf06ce5e1767aab297753b0
MINORA Umberto <umberto.minora@ext.ec.europa.eu>
2021-03-10 21:21:18 +0100

    draft of form group with submit button validation

--- client/src/app/striscioline/striscioline.component.ts
+++ client/src/app/striscioline/striscioline.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-striscioline',
   templateUrl: './striscioline.component.html',
-  styleUrls: ['./striscioline.component.css']
+  styleUrls: ['./striscioline.component.css'],
 })
 export class StrisciolineComponent implements OnInit {
+  strisciolineForm = this.fb.group({
+    hisName: ['', Validators.required],
+    herName: ['', Validators.required]
+  });
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    // Process checkout data here
+    console.log(this.strisciolineForm.value);
   }
-
 }
--- client/src/app/striscioline/striscioline.module.ts
+++ client/src/app/striscioline/striscioline.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StrisciolineComponent } from './striscioline.component';
-
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [StrisciolineComponent],
-  imports: [CommonModule, MatFormFieldModule, MatInputModule],
-  exports: [StrisciolineComponent, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    StrisciolineComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class StrisciolineModule {}
