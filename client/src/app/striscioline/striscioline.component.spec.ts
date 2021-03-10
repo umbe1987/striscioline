e2f63dec5122bbb400686d1e7435a0549bdd40df
MINORA Umberto <umberto.minora@ext.ec.europa.eu>
2021-03-10 12:49:59 +0100

    Angular-CLI updated https://stackoverflow.com/a/65927095/1979665

--- client/src/app/striscioline/striscioline.component.spec.ts
+++ client/src/app/striscioline/striscioline.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StrisciolineComponent } from './striscioline.component';
 
@@ -6,7 +6,7 @@ describe('StrisciolineComponent', () => {
   let component: StrisciolineComponent;
   let fixture: ComponentFixture<StrisciolineComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StrisciolineComponent ]
     })
