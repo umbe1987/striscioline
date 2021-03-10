56cb7025e4dbf93f2b219da6ef70e46b7217ca19
MINORA Umberto <umberto.minora@ext.ec.europa.eu>
2021-03-10 17:18:30 +0100

    added basic input div

--- client/src/app/striscioline/striscioline.module.ts
+++ client/src/app/striscioline/striscioline.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StrisciolineComponent } from './striscioline.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [StrisciolineComponent],
-  imports: [CommonModule],
-  exports: [StrisciolineComponent],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule],
+  exports: [StrisciolineComponent, MatFormFieldModule, MatInputModule],
 })
 export class StrisciolineModule {}
