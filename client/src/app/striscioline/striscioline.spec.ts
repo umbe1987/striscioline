12e2c36cd5d8eefbfd573f83c91b7066798d71c1
MINORA Umberto <umberto.minora@ext.ec.europa.eu>
2021-03-14 15:20:46 +0100

    added temporary static data with questions

---
+++ client/src/app/striscioline/striscioline.spec.ts
@@ -0,0 +1,7 @@
+import { Striscioline } from './striscioline';
+
+describe('Striscioline', () => {
+  it('should create an instance', () => {
+    expect(new Striscioline()).toBeTruthy();
+  });
+});
---
+++ client/src/app/striscioline/striscioline.ts
@@ -0,0 +1,4 @@
+export interface Striscioline {
+  name: string;
+  desc: string;
+}
