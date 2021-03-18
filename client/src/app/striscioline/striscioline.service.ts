d7401a8edd1151d1e1abff78f949a63d601acb46
MINORA Umberto <umberto.minora@ext.ec.europa.eu>
2021-03-18 23:21:23 +0100

    function in striscioline service to format qa from server

--- client/src/app/striscioline/striscioline.service.ts
+++ client/src/app/striscioline/striscioline.service.ts
@@ -11,4 +11,15 @@ export class StrisciolineService {
   getQuestions(): Observable<string[]> {
     return this.http.get<string[]>('./assets/questions.json');
   }
+
+  formatQA(qaStr: string): string[][] {
+    // convert string to array by commas (https://stackoverflow.com/a/2858130/1979665)
+    // convert flat array to pair QA (https://stackoverflow.com/a/44996257/1979665)
+    return qaStr.split(',').reduce((result, _, index, array) => {
+      if (index % 2 === 0) {
+        result.push(array.slice(index, index + 2));
+      }
+      return result;
+    }, []);
+  }
 }
